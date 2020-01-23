abc1c5a860c7cc62f2dbb38b917c42353995a128
therealmbittarelli <maggiekmcclellan@gmail.com>
2020-01-23 13:32:12 -0500

    completed exercise 4

--- array_drills/Array.js
+++ array_drills/Array.js
@@ -1,47 +1,49 @@
 const Memory = require('./memory');
-
+const memory = new Memory();
 
 class Array {
   constructor() {
+
     this.length = 0;
     this._capacity = 0;
-    let memory = new Memory();
     this.ptr = memory.allocate(this.length);
   }
 
   push(value) {
-    
     if (this.length >= this._capacity) {
       this._resize((this.length + 1) * Array.SIZE_RATIO);
     }
 
-    this.memory.set(this.ptr + this.length, value);
+    memory.set(this.ptr + this.length, value);
     this.length++;
   }
 
   _resize(size) {
+
     const oldPtr = this.ptr;
-    this.ptr = this.memory.allocate(size);
+    this.ptr = memory.allocate(size);
     if (this.ptr === null) {
       throw new Error('Out of memory');
     }
-    this.memory.copy(this.ptr, oldPtr, this.length);
-    this.memory.free(oldPtr);
+    memory.copy(this.ptr, oldPtr, this.length);
+    memory.free(oldPtr);
     this._capacity = size;
   }
 
   get(index) {
+
     if (index < 0 || index >= this.length) {
       throw new Error('Index error');
     }
-    return this.memory.get(this.ptr + index);
+    return memory.get(this.ptr + index);
   }
 
   pop() {
     if (this.length == 0) {
       throw new Error('Index error');
     }
-    const value = this.memory.get(this.ptr + this.length - 1);
+
+    const value = memory.get(this.ptr + this.length - 1);
     this.length--;
     return value;
   }
@@ -55,8 +57,8 @@ class Array {
       this._resize((this.length + 1) * Array.SIZE_RATIO);
     }
 
-    this.memory.copy(this.ptr + index + 1, this.ptr + index, this.length - index);
-    this.memory.set(this.ptr + index, value);
+    memory.copy(this.ptr + index + 1, this.ptr + index, this.length - index);
+    memory.set(this.ptr + index, value);
     this.length++;
   }
 
@@ -64,7 +66,9 @@ class Array {
     if (index < 0 || index >= this.length) {
       throw new Error('Index error');
     }
-    this.memory.copy(this.ptr + index, this.ptr + index + 1, this.length - index - 1);
+
+
+    memory.copy(this.ptr + index, this.ptr + index + 1, this.length - index - 1);
     this.length--;
   }
 
--- array_drills/array_drills.js
+++ array_drills/array_drills.js
@@ -1,9 +1,6 @@
-/* eslint-disable no-console */
-//import Array from './Array';
-
 const Array = require('./Array');
 
-function main(){
+function main() {
 
   Array.SIZE_RATIO = 3;
 
@@ -12,8 +9,51 @@ function main(){
 
   // Add an item to the array
   arr.push(3);
+  arr.push(5);
+  arr.push(15);
+  arr.push(19);
+  arr.push(45);
+  arr.push(10);
 
-  console.log(arr);
+  arr.pop();
+  arr.pop();
+  arr.pop();
+
+
+  arr.remove();
+  arr.remove();
+  arr.remove();
+
+  arr.push('tauhida');
+  console.log(arr.get(0));
 }
 
-console.log(main([1,2,3,6,7]));
+main();
+
+// PUSH:
+// What is the length, capacity and memory address of your array with arr.push(3)?:
+// {length: 1, _capacity: 3, ptr: 0}
+
+// What is the length, capacity and memory address of your array? Explain the result 
+// of your program after adding the new lines of code.
+// {length: 6, _capacity: 12, ptr: 0}
+// length of the array is 6 elements long; the initial capacity of 3 was breached, and so 
+// the array resized according to the array's length of 4 (the point at which the length became
+// greater than the capacity) tripled. The address/pointer remains at index 0.
+
+////////////////////////////////////
+
+// POP:
+// What is the length, capacity, and address of your array? Explain the result of your program 
+// after adding the new lines of code:
+// {length: 3, _capacity: 12, ptr: 0}
+// We have popped three elements off the end of the array, and so our length has decreased from
+// 6 to 3 elements.
+
+/////////////////////////////////////
+
+// PRINT, EMPTY, RESIZE:
+// Print first item in the array: 3
+// Empty the array and add just 1 item: arr.push("tauhida"); Print this 1 item that you just added. 
+// What is the result? Can you explain your result?:
+// Result is NaN
